aebc054d6d0808f73e41165c5ad88ced564a83d1
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-06-26 14:21:02 +0200

    feat: Create Task entity and TaskStatus enum

---
+++ src/main/java/com/example/project_management_backend/model/Task.java
@@ -0,0 +1,34 @@
+package com.example.project_management_backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+    
+
+
+    
+}
---
+++ src/main/java/com/example/project_management_backend/model/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.example.project_management_backend.model;
+
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
