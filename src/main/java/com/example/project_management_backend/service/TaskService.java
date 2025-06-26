5fb755bf1942586637cb63523143d5f8240cf649
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-06-26 15:45:00 +0200

    feat: Implement TaskService with business logic for tasks

---
+++ src/main/java/com/example/project_management_backend/service/TaskService.java
@@ -0,0 +1,29 @@
+package com.example.project_management_backend.service;
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.project_management_backend.model.Task;
+import com.example.project_management_backend.model.TaskStatus;
+import com.example.project_management_backend.repository.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+    
+    private final TaskRepository taskRepository;
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Task crateTask(Task task){
+        task.setStatus(TaskStatus.TODO);
+        return taskRepository.save(task);
+    }
+    
+}
